fb9f59239ebd9a38ed3933324e1ff8b138c3298c
三島 <k.mishima@fukuyamaconsul.co.jp>
2025-03-15 02:17:47 +0900

    import エラー修正

--- src/application/query-service/tests/team-by-name-query-service.test.ts
+++ src/application/query-service/tests/team-by-name-query-service.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import type { TeamByNameQueryServiceInterface } from "../team-by-name-query-service";
 
 /**
--- src/application/query-service/tests/team-query-service.test.ts
+++ src/application/query-service/tests/team-query-service.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import type { TeamQueryServiceInterface } from "../team-query-service";
 
 /**
